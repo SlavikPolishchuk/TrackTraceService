52ef80956b37c0b06d429dc486f4f0bc6c6f570e
SlavikPolishchuk <74653495+SlavikPolishchuk@users.noreply.github.com>
2023-04-28 11:08:05 +0300

    add class AsrkFile
    add class AsrkFile

---
+++ TrackTraceService/AsrkFile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackTraceService
+{
+    internal class AsrkFile
+    {
+        public string FileId { get; set; }
+        public string FileName { get; set; }
+        public List<FileBody> fileBody { get; set; }
+        public DateTime CreateDate { get; set; }
+        public int IsSended { get; set; }
+        public DateTime SendDate { get; set; }
+    }
+}
