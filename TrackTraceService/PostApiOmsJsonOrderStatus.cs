1e46ddac35780550625ab03732658d6fe0f2aac8
SlavikPolishchuk <74653495+SlavikPolishchuk@users.noreply.github.com>
2023-04-28 11:17:43 +0300

    add class PostApiOmsJsonOrderStatus

---
+++ TrackTraceService/PostApiOmsJsonOrderStatus.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackTraceService
+{
+    internal class PostApiOmsJsonOrderStatus
+    {
+        public string extr_guid_order { get; set; }
+        public string status_code { get; set; }
+        public string status_date { get; set; }
+        public string reason_ext_id { get; set; }
+        public string location { get; set; }
+        public string exec_subdiv_id { get; set; }
+        public string barcode_gu { get; set; }
+    }
+}
